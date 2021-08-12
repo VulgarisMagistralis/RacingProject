cb952401a5493b53469f105a3d1f2a59d06b83d0
VulgarisMagistralis <cenktuncbilek@gmail.com>
2021-08-12 15:45:11 -0400

    Added toggle functionality on Checkpoint.cs

--- Simple_Race/Assets/Scripts/Checkpoint.cs
+++ Simple_Race/Assets/Scripts/Checkpoint.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 namespace Simple_Race{
     public class Checkpoint : MonoBehaviour{
         public int checkpointIndex;
-        private void Awake(){
-            checkpointIndex = Int32.Parse(Regex.Match(name,@"\d+").Value);
-        }
-        public void EnableCollider(){
-
-        }
-        public void DisableCollider(){
-
-        }
+        private void Awake(){checkpointIndex = Int32.Parse(Regex.Match(name,@"\d+").Value);}
+        public void EnableCollider(){GetComponent<Collider>().enabled = true;}
+        public void DisableCollider(){GetComponent<Collider>().enabled = false;}
     }
 }
